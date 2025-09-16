735e0a63354507764ff6f021f924eabf66495baf
Csyhwaterbear <1316897455@qq.com>
2025-09-15 20:52:31 -0700

    update

--- ibm.adb
+++ ibm.adb
@@ -1,122 +1,39 @@
 package body IBM is
 
-	function Create_Grid(Dim : Natural; Range_Value : Grid_Range_Array) return Grid is
+	function Create_Grid(
+			Dimension	: Dimension_Type;
+			Point_Counts	: array of Positive;
+			Ranges		: Grid_Range_Array
+		) return Grid
+	is
+		Except_Dim : constant Positive := (
+				case Dimension is
+					when D2 => 2,
+					when D3 => 3
+			);
 
-		N : constant Dimension_Count := Dimension_Count(Dim'Length);
-	
+		Result	: Grid (Dimension);
 	begin
-		
-		if N not in 2 .. 3 then
-			raise Grid_Error with "Only accept 2D and 3D grid";
+		if Point_Counts'Length /= Expected_Dim then
+			raise Invalid_Point_Count_Error with 
+				"Expected" & Expected_Dim'Img & " point counts, got" & Point_Counts'Length'Img;
 		end if;
 
-		if Range_Value'Length /= Integer(N) then
-			raise Grid_Error with "Mismatch between range and Dimension";
+		if Ranges'Length /= Expected_Dim then
+			raise Invalid_Range_Error with 
+				"Expected" & Expected_Dim'Img & " ranges, got" & Ranges'Length'Img;
 		end if;
-		
-		declare
-			Result : Grid(N);
-		begin
-			for I in 1 .. N loop
-				Result.Dims_P(I) := Dim(I);
-			end loop;
 
-			for I in 1 .. N loop
-				Result.Range_P(I) := Range_Value(I);
-			end loop;
+		for I in 1 .. Expected_Dim loop
+			Result.Data.Point_Counts(I) := Point_Counts(Point_Counts'First + I - 1);
+		end loop;
 
-			case N is
-				when 2 =>
-					Result.Data_2D := new Float_Array_2D(1..Dim(1), 1..Dim(2));
-					Result.Data_2D.all := (others => (others => 0.0));
-				when 3 =>
-					Result.Data_3D := new Float_Array_3D(1..Dim(1), 1..Dim(2), 1..Dim(3));
-					Result.Data_3D.all := (others => (others => (others => 0.0)));
-			end case;
+		for I in 1 .. Expected_Dim loop
+			Result.Data.Ranges(I) := Ranges(Ranges'First + I - 1);
+		end loop;
 
-			return Result;
-		end;
+		return Result;
+
 	end Create_Grid;
-
-	procedure Free_Grid(G : in out Grid) is
-	begin
-		case G.N is
-			when 2 =>
-				if G.Data_2D /= null then
-					declare
-						Temp : Data_Access_2D := G.Data_2D;
-					begin
-						G.Data_2D := null;
-						Free(Temp);
-					end;
-				end if;
-			when 3 =>
-				if G.Data_3D /= null then
-					declare
-						Temp : Data_Access_3D := G.Data_3D;
-					begin
-						G.Data_3D := null;
-						Free(Temp);
-					end;
-				end if;
-		end case;
-
-	end Free_Grid;
-	
-	function Get_Dimension(G : Grid; Index : Integer) return Integer is
-	begin
-		If Index > Index(G.N) then
-			raise Grid_Error with "Out of Index";
-		end if;
-		return G.Dims_P(Index);
-	end Get_Dimension;
-
-	function Get_Range_Min(G : Grid; Index : Integer) return Float is
-	begin
-		If Index > Index(G.N) then
-			raise Grid_Error with "Out of Index";
-		end if; 
-		return G.Dims_P(Index).Min;
-	end Get_Range_Min;
-
-	function Get_Range_Max(G : Grid; Index : Integer) return Float is
-	begin
-		If Index > Index(G.N) then
-			raise Grid_Error with "Out of Index";
-		end if; 
-		return G.Dims_P(Index).Max;
-	end Get_Range_Max;
-
-	function Get_Data_2D(G : Grid; I, J :Natural) return Float is
-	begin
-		if G.N /= 2 then
-			raise Grid_Error with "This method is for 2D grid only";
-		end if;
-		return G.Data_2D(I, J);
-	end Get_Data_2D;
-
-	procedure Set_Data_2D(G : Grid; I, J :Natural; Value : Float) is
-	begin
-		if G.N /= 2 then
-			raise Grid_Error with "This method is for 2D grid only";
-		end if;
-		G.Data_2D(I, J) := Value;
-	end Set_Data_2D;
-
-	function Get_Data_3D(G : Grid; I, J, K :Natural) return Float is
-	begin
-		if G.N /= 3 then
-			raise Grid_Error with "This method is for 3D grid only";
-		end if;
-		return G.Data_3D(I, J, K);
-	end Get_Data_3D;
-
-	procedure Set_Data_3D(G : Grid; I, J, K :Natural; Value : Float) is
-	begin
-		if G.N /= 3 then
-			raise Grid_Error with "This method is for 3D grid only";
-		end if;
-		G.Data_3D(I, J, K) := Value;
-	end Set_Data_3D;
 
 end IBM;
--- ibm.ads
+++ ibm.ads
@@ -1,50 +1,60 @@
 with Ada.Containers.Vectors;
-with Ada.Numerics.Generic_Real_Arrays;
 
 package IBM is
-	type Dimension_Count is range 2 .. 3;
+	type Dimension_Type is (D2, D3);
+
+	type Coordinate_2D is record
+		X, Y	: Float;
+	end record;
+
+	type Coordinate_3D is record
+		X, Y, Z	: Float;
+	end record;
 
 	type Grid_Range is record
 		Min, Max	: Float;
 	end record;
 
 	type Grid_Range_Array is array (Positive range <>) of Grid_Range;
-	type Dimension_Array is array (Positive range <>) of Integer;
 
-	type Grid(N : Dimension_Count) is tagged private;
-	
-	function Create_Grid(Dim : Dimension_Array; Range_Value : Grid_Range_Array) return Grid;
-	procedure Free_Grid(G : in out Grid);
+	package Coordinate_2D_Vector is new Ada.Containers.Vectors(
+			Index_Type => Positive,
+			Elenment_Type => Coordinate_2D
+		);
 
-	-- Accessors
-	function Get_Dimension(G : Grid; Index : Integer) return Integer;
-	function Get_Range_Min(G : Grid; Index : Integer) return Float;
-	function Get_Range_Max(G : Grid; Index : Integer) return Float;
+	package Coordinate_3D_Vector is new Ada.Containers.Vectors(
+			Index_Type => Positive,
+			Elenment_Type => Coordinate_3D
+		);
 
-	function Get_Data_2D(G : Grid; I, J : Natural) return Float;
-	procedure Set_Data_2D(G : Grid; I, J : Natural; Value : Float);
+	type Grid (Dimension : Dimension_Type) is tagged private;
 
-	function Get_Data_3D(G : Grid; I, J, K : Natural) return Float;
-	procedure Set_Data_3D(G : Grid; I, J, K : Natural; Value : Float);
-	
-	Grid_Error : exception;
+	Invalid_Dimension_Error		: exception;
+	Invalid_Point_Count_Error	: exception;
+	Invalid_Range_Error		: exception;
+
+	function Create_Grid(
+			Dimension	: Dimension_Type;
+			Point_Counts	: array of Positive;
+			Ranges		: Grid_Range_Array
+		) return Grid;
+
 
 private
-	
-	type Float_Array_2D is array (Natural range <>, Natural range <>) of Float;
-	type Float_Array_3D is array (Natural range <>, Natural range <>, Natural range <>) of Float;
-	type Data_Access_2D is access all Float_Array_2D;
-	type Data_Access_3D is access all Float_Array_3D;
-	
-	type Grid (N : Dimension_Count) is tagged record
-		Dims_P  : Dimension_Array(1..N);
-		Range_P : Grid_Range_Array(1..N);
-		case N is
-			when 2 =>
-				Data_2D : Data_Access_2D;
-			when 3 =>
-				Data_3D : Data_Access_3D;
+
+	type Grid_Data (Dimension : Dimension_Type) is record
+		case Dimension is
+			when D2 =>
+				Point_Counts	: array (1 .. 2) of Positive;
+				Ranges		: array (1 .. 2) of Grid_Range;
+			when D3 =>
+				Point_Counts	: array (1 .. 3) of Positive;
+				Ranges		: array (1 .. 3) of Grid_Range;
 		end case;
 	end record;
-end IBM;
 
+	type Grid (Dimension : Dimension_Type) is tagged record
+		Data : Grid_Data (Dimension);
+	end record;
+
+end IBM;
